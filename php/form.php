0d9cf54737b64ee9793aca51dce0ada477e0dd9a
mjablonska <martawaldon@gmail.com>
2018-08-21 10:44:34 +0200

    Another try with php contact form

--- php/form.php
+++ php/form.php
@@ -1,16 +1,10 @@
-<?php
-if(isset($_POST["submit"])) {
-$recipient = "martawaldon@github.io"; //my email
-echo $subject = 'Email message from Weekly Web Dev Form';
-echo $name = $_POST ["Name"];
-echo $email = $_POST["Email"];
-echo  $message = $_POST["yourMessage"];
-
- $mailBody="Name: $name\nEmail: $email\n\n$message"; 
-
- mail($recipient, $subject, $mailBody, "From: $name <$email>");
-
-echo $thankYou="<p>Thank you! We will be in contact with you shortly.</p>";
-
-}
+<?php $name = $_POST['Name'];
+$email = $_POST['Email'];
+$message = $_POST['Message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "marta@poczta.github.io";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
 ?>
